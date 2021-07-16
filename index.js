0fc5a84b997327ead0f9d123f824bf65f65b6088
Lupita Alvarez <lupitaalvarez@Lupitas-MacBook-Air.local>
2021-07-16 15:57:48 -0700

    done

--- index.js
+++ index.js
@@ -208,16 +208,17 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log('task1a', artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
-
+console.log(artists[2].bio);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+ artists[8].name = "Vincent Van Gogh";
+ console.log('task2', artists[8].name);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,10 +229,10 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, index) {
+  return `the artist at index ${array[index].id} is ${array[index].name}`;
 }
-
+console.log('task3', getArtistByIndex(artists, 0));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -243,9 +244,24 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function get20s(array) 
+ { 
+     const new20s = [];
+     for(let i=0; i< array.length; i++)
+     {
+       const array20s = array[i].years.split(" ");
+       let born = parseInt(array20s[0]);
+       let dead = parseInt(array20s[2]);
+       if (born >= 1900 && dead <= 2000)
+       {
+         new20s.push(array[i].name)
+       }  
+     } 
+     return new20s;
+ }
+
+ const ready20s = get20s(artists);
+ console.log('task4', ready20s);
 
 
 
@@ -258,9 +274,13 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
+function removeArtist(array, index) {
+  array.splice(index, 1);
+  return array.length;
   /*Your Code Here*/
 }
+const lengthOf = removeArtist(artists, artists[0]);
+console.log('task5', lengthOf);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -279,9 +299,24 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
+function addArtist(array) {
+   
+    const artistMe = {
+    "id": 20,
+    "name": "Lupita Alvarez", 
+    "years": "1995-07162121",
+    "genre": "Web Design", 
+    "nationality": "American",
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+    array.push(artistMe);
+    return array;
+  }  
+  const newArtist = addArtist(artists);
+  console.log('task6', newArtist);
+
   /*Your Code Here*/
-}
+
 
 
 
@@ -292,9 +327,18 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
+function lotsOfArt(array) {
+  const moreThan = []
+  for( let i = 0; i < array.length; i++){
+    if (array[i].paintings > 100){
+      moreThan.push(array[i].name);
+    }
+  }
+  return moreThan;
   /*Your Code Here*/
 }
+const more100 = lotsOfArt(artists);
+console.log('task7', more100);
 
 
 /* ***** END OF TASKS ***** */
